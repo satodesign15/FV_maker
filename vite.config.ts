10bef22b48d343d4aa920ce49e603cf2ef6750ad
satodesign15 <m.sato.design623777@gmail.com>
2025-12-25 11:12:31 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Vercelに設定した環境変数をクライアントサイドの process.env として動作させる
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env': process.env
   },
   build: {
