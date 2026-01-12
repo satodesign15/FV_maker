bd596c460e66ed8a6bd541ddeab502d041a80de2
satodesign15 <m.sato.design623777@gmail.com>
2026-01-12 15:30:46 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // GitHubリポジトリ名が FV_maker なので、ベースパスを設定
+  base: '/FV_maker/',
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
   },
